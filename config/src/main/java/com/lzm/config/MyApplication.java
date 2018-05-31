8a1e7ddc219064a72cbccf3f91ea433cfd5dd2a6
lzm <334545190@qq.com>
2018-05-31 14:53:03 +0800

    ...

---
+++ config/src/main/java/com/lzm/config/MyApplication.java
@@ -0,0 +1,24 @@
+package com.lzm.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 爱不留
+ * @date 2018年5月25日 下午5:20:05
+ **/
+
+@SpringBootApplication
+@EnableConfigServer
+public class MyApplication {
+
+	
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+
+	}
+
+}
