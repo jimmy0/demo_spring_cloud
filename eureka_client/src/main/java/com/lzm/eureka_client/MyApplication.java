a3e3a2ed08947a41fa0e70ba9a2d8c73a7d4b2e5
lzm <334545190@qq.com>
2018-05-31 16:06:46 +0800

    ...

--- eureka_client/src/main/java/com/lzm/eureka_client/MyApplication.java
+++ eureka_client/src/main/java/com/lzm/eureka_client/MyApplication.java
@@ -1,18 +1,30 @@
 package com.lzm.eureka_client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
-* @author 爱不留   
-* @date 2018年5月29日 下午5:21:19 
-**/
+ * @author 爱不留
+ * @date 2018年5月29日 下午5:21:19
+ **/
 @SpringBootApplication
 @RestController
 public class MyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
-    }
+	@Value("${server.port}")
+	private String port;
+
+	@RequestMapping("/hi")
+	public String home(@RequestParam String name) {
+		return "hi " + name + ",i am from port:" + port;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
 }
