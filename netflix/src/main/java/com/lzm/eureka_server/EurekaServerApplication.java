b284b36ffc90a1204549ff269416d626f22698d1
lzm <334545190@qq.com>
2018-06-04 10:06:46 +0800

    ...

--- netflix/src/main/java/com/lzm/eureka_server/EurekaServerApplication.java
+++ netflix/src/main/java/com/lzm/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.lzm.netflix;
+package com.lzm.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 **/
 @SpringBootApplication
 @EnableEurekaServer
-public class MyApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
